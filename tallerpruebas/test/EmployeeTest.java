a472cdaa14a98606e97243b2d876b567e940ed89
Bryan Alava <bpalava@espol.edu.ec>
2020-08-26 10:23:36 -0500

    Modificacion clase EmployeeTest

--- tallerpruebas/test/EmployeeTest.java
+++ tallerpruebas/test/EmployeeTest.java
@@ -6,6 +6,13 @@ import org.junit.Test;
 
 public class EmployeeTest {
 
+	
+	@Before
+	public Employee createEmployee(float salary, String currency,
+			float bonusPercentage, EmployeeType employeeType) {
+		return new Employee(salary,currency,bonusPercentage,employeeType);
+		
+	}
 	@Test
 	public void test() {
 		fail("Not yet implemented");
